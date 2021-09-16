faabf057f985decae36baccb8dfe3b51e39501b7
unknown <shabaneissa767@gmail.com>
2021-09-16 03:52:21 +0200

    first commit

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Available Scripts
 
+to run projct open two terminals one to start app and other to start api :
+
+1- write in first terminal >> npm start ,
+2- write in second terminal >> firebase emulators:start 
+
 In the project directory, you can run:
 
 ### `npm start`
--- src/Payment/Payment.js
+++ src/Payment/Payment.js
@@ -99,8 +99,8 @@ function Payment() {
                     </div>
                     <div className='payment__address'>
                         <p>{user?.email}</p>
-                        <p>123 React Lane</p>
-                        <p>Los Angeles, CA</p>
+                        <p>Egypt, Cairo</p>
+                       
                     </div>
                 </div>
 
